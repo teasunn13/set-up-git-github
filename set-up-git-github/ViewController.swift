c0cb4f822337277e801694924dd62406c065f871
Tessa <tessasundar@gmail.com>
2021-08-03 13:57:03 -0700

    Initial Commit

---
+++ set-up-git-github/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  set-up-git-github
+//
+//  Created by Tessa Sundar on 8/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
